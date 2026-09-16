6007148e60f500c46b12a8f211784dcbfa1c9459
Davi Scavassa <scavassadavi01@gmail.com>
2026-09-16 20:51:19 -0300

    Adiciona eixo X do Player 2

--- js/04-input.js
+++ js/04-input.js
@@ -3,24 +3,24 @@
    ============================================================ */
 
 const serialInput = {
-  dir1: 'PARADO', dir2: 'PARADO', x1: 'PARADO',
-  axis1: 0, axis2: 0, axisX1: 0,
+  dir1: 'PARADO', dir2: 'PARADO', x1: 'PARADO', x2: 'PARADO',
+  axis1: 0, axis2: 0, axisX1: 0, axisX2: 0,
   b1: 0, b2: 0, b3: 0
 };
 
 const keyInput = {
-  dir1: 'PARADO', dir2: 'PARADO', x1: 'PARADO',
+  dir1: 'PARADO', dir2: 'PARADO', x1: 'PARADO', x2: 'PARADO',
   b1: 0, b2: 0, b3: 0
 };
 
 const input = {
-  dir1: 'PARADO', dir2: 'PARADO', x1: 'PARADO',
-  axis1: 0, axis2: 0, axisX1: 0,
+  dir1: 'PARADO', dir2: 'PARADO', x1: 'PARADO', x2: 'PARADO',
+  axis1: 0, axis2: 0, axisX1: 0, axisX2: 0,
   b1: 0, b2: 0, b3: 0
 };
 
 const prevInput = {
-  dir1: 'PARADO', dir2: 'PARADO', x1: 'PARADO',
+  dir1: 'PARADO', dir2: 'PARADO', x1: 'PARADO', x2: 'PARADO',
   b1: 0, b2: 0, b3: 0
 };
 
@@ -28,6 +28,7 @@ function mergeInput() {
   input.dir1 = keyInput.dir1 !== 'PARADO' ? keyInput.dir1 : serialInput.dir1;
   input.dir2 = keyInput.dir2 !== 'PARADO' ? keyInput.dir2 : serialInput.dir2;
   input.x1 = keyInput.x1 !== 'PARADO' ? keyInput.x1 : serialInput.x1;
+  input.x2 = keyInput.x2 !== 'PARADO' ? keyInput.x2 : serialInput.x2;
 
   input.axis1 = keyInput.dir1 !== 'PARADO'
     ? (keyInput.dir1 === 'CIMA' ? -1 : 1)
@@ -40,6 +41,10 @@ function mergeInput() {
   input.axisX1 = keyInput.x1 !== 'PARADO'
     ? (keyInput.x1 === 'ESQUERDA' ? -1 : 1)
     : serialInput.axisX1;
+
+  input.axisX2 = keyInput.x2 !== 'PARADO'
+    ? (keyInput.x2 === 'ESQUERDA' ? -1 : 1)
+    : serialInput.axisX2;
 
   input.b1 = keyInput.b1 || serialInput.b1;
   input.b2 = keyInput.b2 || serialInput.b2;
@@ -60,28 +65,30 @@ document.addEventListener('keydown', e => {
   }
 
   switch (e.key) {
+    // Player 1 no teclado: W/S + A/D
     case 'w': case 'W': keyInput.dir1 = 'CIMA'; break;
     case 's': case 'S': keyInput.dir1 = 'BAIXO'; break;
     case 'a': case 'A': keyInput.x1 = 'ESQUERDA'; break;
     case 'd': case 'D': keyInput.x1 = 'DIREITA'; break;
+
+    // Player 2 no teclado: setas
     case 'ArrowUp':
       e.preventDefault();
-      keyInput.dir1 = 'CIMA';
       keyInput.dir2 = 'CIMA';
       break;
     case 'ArrowDown':
       e.preventDefault();
-      keyInput.dir1 = 'BAIXO';
       keyInput.dir2 = 'BAIXO';
       break;
     case 'ArrowLeft':
       e.preventDefault();
-      keyInput.x1 = 'ESQUERDA';
+      keyInput.x2 = 'ESQUERDA';
       break;
     case 'ArrowRight':
       e.preventDefault();
-      keyInput.x1 = 'DIREITA';
+      keyInput.x2 = 'DIREITA';
       break;
+
     case 'Enter': case ' ':
       e.preventDefault();
       keyInput.b2 = 1;
@@ -103,11 +110,10 @@ document.addEventListener('keyup', e => {
       keyInput.x1 = 'PARADO';
       break;
     case 'ArrowUp': case 'ArrowDown':
-      keyInput.dir1 = 'PARADO';
       keyInput.dir2 = 'PARADO';
       break;
     case 'ArrowLeft': case 'ArrowRight':
-      keyInput.x1 = 'PARADO';
+      keyInput.x2 = 'PARADO';
       break;
     case 'Enter': case ' ': keyInput.b2 = 0; break;
     case 'Escape': case 'Backspace': keyInput.b1 = 0; break;
