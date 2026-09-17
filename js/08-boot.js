409d3a635ddef7581cd362bf2c60a5a745ebd175
Davi Scavassa <scavassadavi01@gmail.com>
2026-09-16 21:04:52 -0300

    Sincroniza textos dos novos controles no carregamento

--- js/08-boot.js
+++ js/08-boot.js
@@ -6,5 +6,6 @@ renderRecords('1P');
 initSerialPanel();
 game.p1 = newPlayer(state.p1Special, false);
 game.p2 = newPlayer(state.p2Special, true);
+updateStaticControlLabels();
 updateNavFocus();
 requestAnimationFrame(loop);
